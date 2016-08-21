e9d39e0b73c2f193ece00e219f8365a42e7f75bf
Maxime Junger <maxime@junger.io>
2016-08-21 12:57:33 +0200

    New completion type to handle all callbacks the same way

---
+++ IntraEpitech/Utils/CompletionHandlerType.swift
@@ -0,0 +1,21 @@
+//
+//  CompletionHandlerType.swift
+//  IntraEpitech
+//
+//  Created by Maxime Junger on 20/08/16.
+//  Copyright © 2016 Maxime Junger. All rights reserved.
+//
+
+import Foundation
+
+typealias CompletionHandlerType = (Result) -> Void
+
+enum Result {
+	case Success(AnyObject?)
+	case Failure(type: Error, message: String?)
+}
+
+enum Error: ErrorType {
+	case APIError
+	case AuthenticationFailure
+}
