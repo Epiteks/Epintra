f2b6639db0e6ce7c7825bd0005bb4b536c5e4b50
Maxime Junger <maxime@junger.io>
2016-09-05 01:26:03 +0200

    Settings comments removed / Updated style

--- IntraEpitech/ViewControllers/Settings/SettingsViewController.swift
+++ IntraEpitech/ViewControllers/Settings/SettingsViewController.swift
@@ -11,11 +11,11 @@ import MessageUI
 import StoreKit
 
 class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, MFMailComposeViewControllerDelegate {
-
+	
 	@IBOutlet weak var menuButton: UIBarButtonItem!
-
+	
 	var products: [SKProduct]!
-
+	
 	let _settingsItems = [
 		["DefaultCalendar"],
 		["DownloadOnData"],
@@ -23,65 +23,32 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
 		["Disconnect"]
 	]
 	@IBOutlet weak var _tableView: UITableView!
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-
-				//self.title = NSLocalizedString("Settings", comment: "")
-//self.navigationItem.backBarButtonItem?.tintColor = UIColor.redColor()
-
-	}
-
+	
 	override func awakeFromNib() {
 		self.title = NSLocalizedString("Settings", comment: "")
 	}
-
-	//	func productTest() {
-	//		products = [SKProduct]()
-	//
-	//		IntraProduct.store.requestProductsWithCompletionHandler { success, products in
-	//			if success {
-	//				self.products = products
-	//				for tmp in products {
-	//					print(tmp.productIdentifier)
-	//					print(tmp.price)
-	//				}
-	//			}
-	//			print("hello")
-	//
-	//			for tmp in self.products {
-	//				print(tmp.productIdentifier)
-	//				print(tmp.price)
-	//			}
-	//		}
-	//	}
-
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-
+	
 	override func viewDidAppear(animated: Bool) {
 		self._tableView.reloadData()
 	}
-
+	
 	func numberOfSectionsInTableView(tableView: UITableView) -> Int {
 		return _settingsItems.count
 	}
-
+	
 	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return _settingsItems[section].count
 	}
-
+	
 	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-
+		
 		var cell = UITableViewCell()
-
+		
 		cell.accessoryType = .None
-
+		
 		if (_settingsItems[indexPath.section][indexPath.row] == "Disconnect") {
-			cell.textLabel?.textColor = UIColor.redColor()
+			cell.textLabel?.textAlignment = .Center
+			cell.textLabel?.textColor = UIColor(hexString: "#c0392bff")
 			cell.textLabel?.text = NSLocalizedString(_settingsItems[indexPath.section][indexPath.row], comment: "")
 		} else if (_settingsItems[indexPath.section][indexPath.row] == "DownloadOnData") {
 			cell = tableView.dequeueReusableCellWithIdentifier("switchCell")!
@@ -92,7 +59,7 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
 			uiswitch.addTarget(self, action: #selector(SettingsViewController.switchClicked(_:)), forControlEvents: .ValueChanged)
 			label.text = NSLocalizedString(_settingsItems[indexPath.section][indexPath.row], comment: "")
 		} else if (_settingsItems[indexPath.section][indexPath.row] == "DefaultCalendar") {
-
+			
 			if let cal = ApplicationManager.sharedInstance.defaultCalendar {
 				cell = tableView.dequeueReusableCellWithIdentifier("CalendarCell")!
 				let titleLabel = cell.viewWithTag(1) as! UILabel
@@ -100,40 +67,40 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
 				titleLabel.text = NSLocalizedString("DefaultCalendar", comment: "")
 				calendarName.text = cal
 			} else {
-				cell.accessoryType = .DisclosureIndicator
 				cell.textLabel?.text = NSLocalizedString("DefaultCalendar", comment: "")
 			}
-
+			cell.accessoryType = .DisclosureIndicator
+			
 		} else if (_settingsItems[indexPath.section][indexPath.row] == "ThirdParty") {
 			cell.accessoryType = .DisclosureIndicator
 			cell.textLabel?.text = NSLocalizedString("ThirdParty", comment: "")
 		} else if (_settingsItems[indexPath.section][indexPath.row] == "BugQuestion") {
 			cell.textLabel?.text = NSLocalizedString("BugQuestion", comment: "")
 		}
-
+		
 		return cell
 	}
-
+	
 	/*
 	// MARK: - Navigation
-
+	
 	// In a storyboard-based application, you will often want to do a little preparation before navigation
 	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
 	// Get the new view controller using segue.destinationViewController.
 	// Pass the selected object to the new view controller.
 	}
 	*/
-
+	
 	func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-
+		
 		tableView.deselectRowAtIndexPath(indexPath, animated: true)
-
+		
 		if (_settingsItems[indexPath.section][indexPath.row] == "Disconnect") {
 			disconnectAction()
 		} else if (_settingsItems[indexPath.section][indexPath.row] == "DefaultCalendar") {
-
+			
 			performSegueWithIdentifier("SelectCalendarSegue", sender: self)
-
+			
 		} else if (_settingsItems[indexPath.section][indexPath.row] == "ThirdParty") {
 			showThirdParty()
 		} else if (_settingsItems[indexPath.section][indexPath.row] == "BugQuestion") {
@@ -141,44 +108,44 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
 		} else {
 			//productTest()
 		}
-
-
-
+		
+		
+		
 	}
-
+	
 	func disconnectAction() {
-
+		
 		let confirmationMenu = UIAlertController(title: nil, message: NSLocalizedString("WantToDisconnect", comment: ""), preferredStyle: .ActionSheet)
-
+		
 		let disconnect = UIAlertAction(title: NSLocalizedString("Disconnect", comment: ""), style: .Destructive, handler: {
 			(alert: UIAlertAction!) -> Void in
-
+			
 			UserPreferences.deleteData()
-
+			
 			let storyboard = UIStoryboard(name: "ConnexionStoryboard", bundle: nil)
 			let vc = storyboard.instantiateInitialViewController()
 			self.presentViewController(vc!, animated: true, completion: nil)
 		})
-
+		
 		let cancelAction = UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: .Cancel, handler: {
 			(alert: UIAlertAction!) -> Void in
 			print("Cancelled")
 		})
-
+		
 		confirmationMenu.addAction(disconnect)
 		confirmationMenu.addAction(cancelAction)
-
+		
 		self.presentViewController(confirmationMenu, animated: true, completion: nil)
 	}
-
+	
 	func switchClicked(sender: UISwitch?) {
 		print(sender?.on)
 		ApplicationManager.sharedInstance.canDownload = sender!.on
 		UserPreferences.saveWantToDownloadImage(sender!.on)
 	}
-
+	
 	func sendEmail() {
-
+		
 		if (MFMailComposeViewController.canSendMail()) {
 			let mailComposerVC = MFMailComposeViewController()
 			mailComposerVC.mailComposeDelegate = self
@@ -187,13 +154,13 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
 			mailComposerVC.setMessageBody(SystemUtils.getAllMailData(), isHTML: false)
 			self.presentViewController(mailComposerVC, animated: true, completion: nil)
 		}
-
+		
 	}
-
+	
 	func showThirdParty() {
 		self.performSegueWithIdentifier("webViewSegue", sender: self)
 	}
-
+	
 	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
 		if (segue.identifier == "webViewSegue") {
 			let vc: WebViewViewController = segue.destinationViewController as! WebViewViewController
@@ -202,7 +169,7 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
 			vc.isUrl = false
 		}
 	}
-
+	
 	func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
 		controller.dismissViewControllerAnimated(true, completion: nil)
 	}
