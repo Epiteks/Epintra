f0ea6977af9e207d5cae86ad296193ea35c84f0d
Maxime Junger <maxime@junger.io>
2016-09-06 23:26:27 +0200

    Loading data table cell

---
+++ IntraEpitech/Views/LoadingDataTableViewCell/LoadingDataTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LoadingDataTableViewCell.swift
+//  IntraEpitech
+//
+//  Created by Maxime Junger on 06/09/16.
+//  Copyright © 2016 Maxime Junger. All rights reserved.
+//
+
+import UIKit
+
+class LoadingDataTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
