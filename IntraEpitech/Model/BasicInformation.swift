5e44cafa78fbb8e7bf4d1ed9b7acdebe03f74f58
Maxime Junger <maxime@junger.io>
2017-01-24 11:33:44 +0000

    gitignore

---
+++ IntraEpitech/Model/BasicInformation.swift
@@ -0,0 +1,9 @@
+//
+//  BasicInformation.swift
+//  IntraEpitech
+//
+//  Created by Maxime Junger on 23/01/2017.
+//  Copyright © 2017 Maxime Junger. All rights reserved.
+//
+
+import Foundation
---
+++ IntraEpitech/Utils/KeychainUtil.swift
@@ -0,0 +1,9 @@
+//
+//  KeychainUtil.swift
+//  IntraEpitech
+//
+//  Created by Maxime Junger on 24/01/2017.
+//  Copyright © 2017 Maxime Junger. All rights reserved.
+//
+
+import Foundation
