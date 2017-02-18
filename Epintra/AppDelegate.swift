c0e1d4bbdedb5ab6b16545640f539ce24cdc55ac
Hugo SCHOCH <schoch.hugo@gmail.com>
2017-02-19 02:11:59 +0700

    👇🏻 Start Quick Actions with Force Touch integration

--- Epintra/AppDelegate.swift
+++ Epintra/AppDelegate.swift
@@ -14,12 +14,17 @@ let log = SwiftyBeaver.self
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-	
+
+	enum QuickActionType {
+		case showUserProfile
+	}
+
 	var window: UIWindow?
-	
+
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
 		// Configure tracker from GoogleService-Info.plist.
+
 		let navigationBarAppearace = UINavigationBar.appearance()
 		navigationBarAppearace.tintColor = UIUtils.backgroundColor
 		
@@ -37,7 +42,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		
 		return true
 	}
-	
+
+	func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+		switch shortcutItem.type {
+		case "ShowUserProfile" :
+			lauchQuickAction(.showUserProfile)
+		default:
+			break
+		}
+		completionHandler(true)
+	}
+
 	func applicationWillResignActive(_ application: UIApplication) {
 		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
 		// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -55,8 +70,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func applicationDidBecomeActive(_ application: UIApplication) {
 		// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 	}
-	
+
 	func applicationWillTerminate(_ application: UIApplication) {
 		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 	}
+
+	func lauchQuickAction(_ type: QuickActionType) {
+			switch type {
+			case .showUserProfile:
+				// Show Profile Tab
+				break
+			default:
+				break
+		}
+	}
+
 }
