11ac09e3d60021afd1de8eebb4c99e7c3abfe880
Maxime Junger <maxime@junger.io>
2017-04-06 22:13:43 +0100

    Added PDF reader for nearly all links We still use the webview when it another link than a pdf one

--- Epintra/ViewControllers/School/Projects/ProjectDetailsViewController.swift
+++ Epintra/ViewControllers/School/Projects/ProjectDetailsViewController.swift
@@ -10,53 +10,53 @@ import UIKit
 import PDFReader
 
 class ProjectDetailsViewController: UIViewController {
-    
+
     @IBOutlet weak var masterProfileImage: UIImageView!
     @IBOutlet weak var masterNameLabel: UILabel!
     @IBOutlet weak var projectEndLabel: UILabel!
     @IBOutlet weak var projectEndProgressView: UIProgressView!
     @IBOutlet weak var projectTableView: UITableView!
-    
+
     // Project data
     var project: Project!
-    
+
     // Project members
     var members = [User]()
-    
+
     // Project files
     var files = [File]()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Do any additional setup after loading the view.
-        
+
         self.title = self.project.actiTitle
-        
+
         if let end = self.project.end {
             self.projectEndLabel.text = end.toDate().toProjectEnding()
         }
-        
+
         if let files = self.project.files {
             self.files = files
         }
-        
+
         self.setMasterStudentData()
         self.setProgressView()
-        
+
         self.projectTableView.estimatedRowHeight = 60
         self.projectTableView.rowHeight = UITableViewAutomaticDimension
-        
+
         self.projectTableView.register(UINib(nibName: "ProjectDetailsUserTableViewCell", bundle: nil), forCellReuseIdentifier: "userCell")
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     func setMasterStudentData() {
-        
+
         if (self.project?.isRegistered() == true) {
             let grp = self.project?.findGroup((project?.userProjectCode!)!)
             self.members = (grp?.members)!
@@ -68,18 +68,18 @@ class ProjectDetailsViewController: UIViewController {
         self.masterProfileImage.cropToSquare()
         self.masterProfileImage.toCircle()
     }
-    
+
     func setProgressView() {
-        
+
         if let begin = self.project?.begin?.toDate(), let end = self.project?.end?.toDate() {
-            
+
             let today = Date()
-            
+
             let totalTime = end.timeIntervalSince(begin)
             let currentTime = end.timeIntervalSince(today)
-            
+
             let percent = 1 - (currentTime * 100 / totalTime) / 100
-            
+
             if end < today {
                 self.projectEndProgressView.setProgress(1.0, animated: true)
                 self.projectEndProgressView.progressTintColor = UIUtils.planningRedColor
@@ -93,50 +93,50 @@ class ProjectDetailsViewController: UIViewController {
             }
         }
     }
-    
+
     func setUIIfRegistered(_ grp: ProjectGroup?) {
-        
+
         if let profileImageURL = grp?.master?.imageUrl {
             self.masterProfileImage.downloadProfileImage(fromURL: URL(string: profileImageURL)!)
         }
     }
-    
+
 }
 
 extension ProjectDetailsViewController: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 2
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return section == 0 ? self.files.count : self.members.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+
         return indexPath.section == 0 ? self.getFileCell(for: self.files[indexPath.row]) : self.getStudentCell(for: self.members[indexPath.row])
     }
-    
+
     func getFileCell(for file: File) -> UITableViewCell {
-        
+
         let cell = UITableViewCell()//.. self.projectTableView.dequeueReusableCell(withIdentifier: "fileCell")!
-        
+
         if let title = file.title {
             cell.textLabel?.text = title
         }
 
         cell.accessoryType = .disclosureIndicator
-        
+
         return cell
     }
-    
+
     func getStudentCell(for member: User) -> UITableViewCell {
-        
+
         let cell = self.projectTableView.dequeueReusableCell(withIdentifier: "userCell") as! ProjectDetailsUserTableViewCell
 
         cell.userProfileImageView.image = UIImage(named: "userProfile")
         cell.userNameLabel.text = member.title
-        
+
         if (member.status == "confirmed") {
             cell.accessoryType = .checkmark
             cell.tintColor = UIUtils.planningGreenColor
@@ -145,17 +145,17 @@ extension ProjectDetailsViewController: UITableViewDataSource {
             cell.statusImageView.image = UIImage(named: "Delete")?.withRenderingMode(UIImageRenderingMode.alwaysTemplate)
             cell.statusImageView.tintColor = UIUtils.planningRedColor
         }
-        
+
         if let profileImageURL = member.imageUrl, let url = URL(string: profileImageURL) {
             cell.userProfileImageView.downloadProfileImage(fromURL: url)
         }
-        
+
         return cell
     }
 }
 
 extension ProjectDetailsViewController: UITableViewDelegate {
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         if indexPath.section == 0 {
@@ -163,7 +163,7 @@ extension ProjectDetailsViewController: UITableViewDelegate {
             open(file: file)
         }
     }
-    
+
     func open(file: File) {
 
         guard let urlString = file.url, let fileURL = URL(string: urlString) else {
@@ -171,21 +171,22 @@ extension ProjectDetailsViewController: UITableViewDelegate {
             return
         }
 
-        RequestManager.downloadData(fromURL: fileURL) { result in
-            switch result {
-            case .success(let data):
-                let document = PDFDocument(fileData: data, fileName: "Sample PDF")!
-                let readerController = PDFViewController.createNew(with: document, title: file.title ?? "", actionStyle: .activitySheet, isThumbnailsEnabled: false)
-
-                self.navigationController?.pushViewController(readerController, animated: true)
-            case .failure(let err):
-                print(err)
+        if !urlString.contains(".pdf") {
+            let vc = WebViewController()
+            vc.file = file
+            self.show(vc, sender: self)
+        } else {
+            RequestManager.downloadData(fromURL: fileURL) { result in
+                switch result {
+                case .success(let data):
+                    let document = PDFDocument(fileData: data, fileName: file.title ?? "")!
+                    let readerController = PDFViewController.createNew(with: document, actionStyle: .activitySheet, isThumbnailsEnabled: false)
+                    self.navigationController?.pushViewController(readerController, animated: true)
+                case .failure(let err):
+                    log.error("Error retrieving pdf file : \(err.type) \(err.message ?? "")")
+                }
             }
         }
-
-//        let vc = WebViewController()
-//        vc.file = file
-//        self.show(vc, sender: self)
     }
     
 }
